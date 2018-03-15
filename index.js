5c882f26582fb997ec1e923152478010c700ee45
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-15 18:45:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return  Object.assign({}, object, {[key]:value})
 }
 
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.[key] = value
+  return object
+}
